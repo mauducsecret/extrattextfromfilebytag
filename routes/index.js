b64cc0c0f4f899b9c6fd0021c5eeeab474bd6084
Nguyen Duc <kiniembancuoilop@gmail.com>
2020-03-19 10:46:13 +0700

    Commit code extrat text to get string

---
+++ routes/index.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+
+
+
+function getFiles (dir, files_){
+  files_ = files_ || [];
+  var files = fs.readdirSync(dir);
+  for (var i in files){
+      var name = dir + '/' + files[i];
+      if (fs.statSync(name).isDirectory()){
+          getFiles(name, files_);
+      } else {
+          if(name.includes(".tsx"))
+          files_.push(name);
+      }
+  }
+  return files_;
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  let dirCont = fs.readdirSync('C:\\Users\\kinie\\Documents\\app-web-skeleton');
+  let files = dirCont.filter( function( elm ) {return elm.match(/.*\.(tsx?tsx)/ig);});
+  files = getFiles('C:\\Users\\kinie\\Documents\\app-web-skeleton');
+  let strings = [];
+  files.forEach(file => {
+    // looping code
+    const str = fs.readFileSync(file, 'utf8');
+    if(str == null) {
+      return;
+    }
+    var result = str.match(/<Trans>(.*?)<\/Trans>/g);
+    if(result == null) {
+      return;
+    }
+    result.map(function(val){
+      strings.push(val);
+      
+      return;
+    });
+  });
+  strings = strings.filter(function(elem, pos) {
+    return strings.indexOf(elem) == pos;
+  });
+  try {
+    var stats = fs.statSync(number);
+    fs.unlinkSync('result.txt');
+  }
+  catch(err) {
+      console.log('it does not exist');
+  }
+  
+  strings.forEach(str => {
+    fs.appendFileSync('result.txt', str +'\n', (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  });
+  
+  
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
