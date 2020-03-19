c83aa70d15cdb896834000c607f4ed0913c4f2c4
DucNM <ducnm@splus-software.com.vn>
2020-03-19 18:23:20 +0700

    Commit Handel Translations En to VN

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
-
-
-
+const translate = require('translate');
 
 function getFiles (dir, files_){
   files_ = files_ || [];
@@ -23,9 +21,9 @@ function getFiles (dir, files_){
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-  let dirCont = fs.readdirSync('C:\\Users\\kinie\\Documents\\app-web-skeleton');
+  let dirCont = fs.readdirSync('/home/ducnm/Desktop/Front End');
   let files = dirCont.filter( function( elm ) {return elm.match(/.*\.(tsx?tsx)/ig);});
-  files = getFiles('C:\\Users\\kinie\\Documents\\app-web-skeleton');
+  files = getFiles('/home/ducnm/Desktop/Front End');
   let strings = [];
   files.forEach(file => {
     // looping code
@@ -38,6 +36,8 @@ router.get('/', function(req, res, next) {
       return;
     }
     result.map(function(val){
+      val = val.replace("<Trans>","");
+      val = val.replace("<\/Trans>","");
       strings.push(val);
       
       return;
@@ -47,18 +47,25 @@ router.get('/', function(req, res, next) {
     return strings.indexOf(elem) == pos;
   });
   try {
-    var stats = fs.statSync(number);
+    var stats = fs.statSync('result.txt');
     fs.unlinkSync('result.txt');
   }
   catch(err) {
       console.log('it does not exist');
   }
-  
+  translate.engine = 'yandex';
+
+  translate.key = 'trnsl.1.1.20200319T085143Z.d2710a7eafd76b2f.3595dfacce2303402350097531754097276a3317';
   strings.forEach(str => {
-    fs.appendFileSync('result.txt', str +'\n', (err) => {
-      if (err) throw err;
-      console.log('The file has been saved!');
+    translate(str, { from: 'en', to: 'vi' }).then(text => {
+      str = '"' + str + '"' + ':' + '"' + text + '"' + ",";
+      fs.appendFileSync('result.txt', str +'\n', (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
     });
+
+    
   });
   
   
